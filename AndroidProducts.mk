fe7eae5003752f3efd8e8c0ffb88ec07d2b15886
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-12 21:09:36 +0530

    gram: arrow -> havoc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2020 The Havoc-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_gram.mk
+    $(LOCAL_DIR)/havoc_gram.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_gram-user \
-    arrow_gram-userdebug \
-    arrow_gram-eng
+    havoc_gram-user \
+    havoc_gram-userdebug \
+    havoc_gram-eng
--- havoc_gram.mk
+++ havoc_gram.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2020 The Havoc-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,20 +7,19 @@
 # Inherit from gram device
 $(call inherit-product, device/xiaomi/gram/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Havoc-OS stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+
+HAVOC_BUILD_TYPE := Official
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_gram
+PRODUCT_NAME := havoc_gram
 PRODUCT_DEVICE := gram
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Poco M2 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
